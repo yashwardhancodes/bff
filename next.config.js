0a0e2be9ee83ffcb931698d4ccb13c02a3aadd9d
yashwardhancodes <143013221+yashwardhancodes@users.noreply.github.com>
2025-09-28 21:57:29 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  // Remove or comment out 'output: "export"'
+  // Remove 'images: { unoptimized: true }' if you want image optimization
 };
-
 module.exports = nextConfig;
